3c3b6bb208c2fc3d7cb93d3c2d1ac3ff58232eb8
jwjwjw123 <e0321129@u.nus.edu>
2019-01-09 20:57:47 +0800

    Add project files.

---
+++ Team7ADProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Team7ADProject.MvcApplication" Language="C#" %>
---
+++ Team7ADProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team7ADProject.Startup))]
+namespace Team7ADProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
