ee3d938704901f8c2de1370628e79dd3e700b1c1
jwjwjw123 <e0321129@u.nus.edu>
2019-01-12 23:07:11 +0800

    added stuff

---
+++ Team7ADProject/Controllers/ManageRepController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team7ADProject.Controllers
+{
+    public class ManageRepController : Controller
+    {
+        // GET: ManageRep
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
