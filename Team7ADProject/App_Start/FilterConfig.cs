a918aa5ddfe20ed7c3c69a32515b420a43bfb145
Li Heng Teh <e0320674@u.nus.edu>
2019-01-15 05:15:34 +0800

    applied authorization site wide, optimization

--- Team7ADProject/App_Start/FilterConfig.cs
+++ Team7ADProject/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Team7ADProject
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
--- Team7ADProject/Controllers/HomeController.cs
+++ Team7ADProject/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Team7ADProject.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
