dce8fc4df61960b77732137ce62118f32e3430fd
jwjwjw123 <e0321129@u.nus.edu>
2019-01-14 14:01:21 +0800

    Added controllers

---
+++ Team7ADProject/Controllers/ApproveOrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team7ADProject.Controllers
+{
+    public class ApproveOrderController : Controller
+    {
+        // GET: ApproveOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Team7ADProject/Controllers/RaiseAdjustmentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team7ADProject.Controllers
+{
+    //For SC to raise adjustment
+    public class RaiseAdjustmentController : Controller
+    {
+        // GET: RaiseAdjustment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
