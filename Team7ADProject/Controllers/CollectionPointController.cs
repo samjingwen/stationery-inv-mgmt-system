2b78db35a5c96990fdc8fab3c7531f0b88432af3
jwjwjw123 <e0321129@u.nus.edu>
2019-01-14 21:33:57 +0800

    added change collection point for rep/head

--- Team7ADProject/Controllers/CollectionPointController.cs
+++ Team7ADProject/Controllers/CollectionPointController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Team7ADProject.Entities;
+using Team7ADProject.ViewModels;
 
 //Author: Sam Jing Wen
 
@@ -18,12 +19,28 @@ namespace Team7ADProject.Controllers
         public ActionResult Index()
         {
             LogicDB context = new LogicDB();
+            CollectionPointViewModel model = new CollectionPointViewModel();
             string userId = User.Identity.GetUserId();
             string cpName = context.AspNetUsers.FirstOrDefault(x => x.Id == userId).Department.CollectionPoint.CollectionDescription;
             ViewBag.cpName = cpName;
-            var list = context.CollectionPoint.ToList();
-            return View(list);
+            return View(model);
         }
+
+        [HttpPost]
+        [Authorize(Roles = "Department Head, Department Representative")]
+        public string UpdateCollectionPoint(CollectionPointViewModel model)
+        {
+            LogicDB context = new LogicDB();
+            string userId = User.Identity.GetUserId();
+            var query = context.AspNetUsers.FirstOrDefault(x => x.Id == userId);
+            var dept = query.Department;
+            int cpId = Convert.ToInt32(model.SelectedCP);
+            var collPoint = context.CollectionPoint.FirstOrDefault(x => x.CollectionPointId == cpId);
+            dept.CollectionPoint = collPoint;
+            context.SaveChanges();
+            return "Update Successful";
+        }
+
         #endregion
 
     }
