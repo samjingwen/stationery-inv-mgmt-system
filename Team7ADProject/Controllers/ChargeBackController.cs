94a71d5ec0afa1615f115c4c1560016bf9d92164
Chanylec <e0321097@u.nus.edu>
2019-01-15 22:27:50 +0800

    ChargeBack Details

--- Team7ADProject/Controllers/ChargeBackController.cs
+++ Team7ADProject/Controllers/ChargeBackController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using Team7ADProject.Entities;
 using Team7ADProject.ViewModels;
+using Team7ADProject.ViewModels.ChargeBack;
 
 namespace Team7ADProject.Controllers
 {
@@ -22,15 +23,19 @@ namespace Team7ADProject.Controllers
         {             
                 LogicDB context = new LogicDB();
                 String UID = User.Identity.GetUserId();
-                String DID = context.Department.Where(x=>x.DepartmentHeadId==UID).First().DepartmentId;
+                String DID = context.Department.
+                Where(x=>x.DepartmentHeadId==UID).
+                First().DepartmentId;
             
-                var chargeback = context.Disbursement.Where(x=>x.DepartmentId == DID).ToList().Select(x => new DepartmentChargeBackViewModel()
+                var chargeback = context.Disbursement.
+                Where(x=>x.DepartmentId == DID).ToList().
+                Select(x => new DepartmentChargeBackViewModel()
                 {
                     DisbursementId = x.DisbursementId,
                     DisbursementNo = x.DisbursementNo,
                     DepartmentId = x.DepartmentId,
                     Department = x.Department,
-                    AcknowledgedBy = x.AcknowledgedBy,
+                    AcknowledgedBy = context.AspNetUsers.Where(y=>y.Id== x.AcknowledgedBy).First().EmployeeName,                    
                     DisbursedBy = x.DisbursedBy,
                     Date = x.Date,
                     RequestId = x.RequestId,
@@ -44,6 +49,31 @@ namespace Team7ADProject.Controllers
 
                 return View(chargeback);
 
+        }
+
+        [Authorize(Roles = "Department Head")]
+        public ActionResult ChargeBackDetails(String id)
+        {
+            LogicDB context = new LogicDB();
+            var disbursement = context.TransactionDetail.Where(x => x.TransactionRef == id).ToList().
+                Select(x => new DisbursementDetailsViewModel()
+                {
+                    DisbursementId = x.TransactionRef,
+                    DisbursementNo = x.Disbursement.DisbursementNo,
+                    DisbAcknowledgedBy = x.Disbursement.AspNetUsers1.EmployeeName,
+                    DisbDate = x.Disbursement.Date,
+                    DisbStatus = x.Disbursement.Status,
+                    RequestId = x.Disbursement.RequestId,
+                    RequestedBy = x.Disbursement.StationeryRequest.AspNetUsers1.EmployeeName,
+                    RequestDepartmentId = x.Disbursement.StationeryRequest.DepartmentId,
+                    ReqStatus = x.Disbursement.StationeryRequest.Status,
+                    RequestDate = x.Disbursement.StationeryRequest.RequestDate,
+                    ItemDesc = context.Stationery.Where(y=>y.ItemId == x.ItemId).First().Description,
+                    Quantity = x.Quantity,
+                    Amount = (x.Quantity)*(x.UnitPrice)
+                    
+                });
+            return View(disbursement);
         }
         #endregion
 
---
+++ Team7ADProject/ViewModels/ChargeBack/DisbursementDetailsViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Team7ADProject.Entities;
+
+namespace Team7ADProject.ViewModels.ChargeBack
+{//Author: Elaine Chan
+    public class DisbursementDetailsViewModel
+    {
+        #region From StationeryRequest
+        [Key]
+        [StringLength(10)]
+        public string RequestId { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string RequestedBy { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public string RequestDepartmentId { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string ReqStatus { get; set; }
+
+        public DateTime RequestDate { get; set; }
+
+        public virtual Department Department { get; set; }
+
+        #endregion
+
+        #region From Disbursement
+        [StringLength(10)]
+        public string DisbursementId { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string DisbursementNo { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string DisbAcknowledgedBy { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DisbDate { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string DisbStatus { get; set; }
+        #endregion
+
+        #region From TransactionDetails
+
+        [Required]
+        [StringLength(4)]
+        public string ItemDesc { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "numeric")]
+        public decimal? Amount { get; set; }
+        #endregion
+    }
+}
