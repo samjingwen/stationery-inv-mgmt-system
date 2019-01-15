aea1493804920d54540671b3324b29ca773f01a8
jwjwjw123 <e0321129@u.nus.edu>
2019-01-15 11:22:56 +0800

    added datepicker

--- Team7ADProject/Controllers/ManagePointController.cs
+++ Team7ADProject/Controllers/ManagePointController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -28,11 +29,50 @@ namespace Team7ADProject.Controllers
         }
 
 
-        public ActionResult GetData()
+        //public ActionResult GetData()
+        //{
+        //    LogicDB context = new LogicDB();
+        //    var cpList = (from x in context.CollectionPoint select new { x.CollectionPointId, x.CollectionDescription, x.Time }).ToList();
+        //    return Json(new { data = cpList }, JsonRequestBehavior.AllowGet);
+        //}
+
+        public ActionResult SaveCP(int id, string propertyName, string value)
         {
-            LogicDB context = new LogicDB();
-            var cpList = (from x in context.CollectionPoint select new { x.CollectionPointId, x.CollectionDescription, x.Time }).ToList();
-            return Json(new { data = cpList }, JsonRequestBehavior.AllowGet);
+            var status = false;
+            var message = "";
+            //Update data to database
+            using (LogicDB context = new LogicDB())
+            {
+                var CollPoint = context.CollectionPoint.Find(id);
+
+                object updateValue = value;
+                bool isValid = true;
+                
+                if(propertyName == "CollectionDescription")
+                {
+
+                }
+                else if(propertyName == "Time")
+                {
+                    //DateTime time;
+                    //if (DateTime.TryParse)
+                }
+
+                if (CollPoint != null)
+                {
+                    context.Entry(CollPoint).Property(propertyName).CurrentValue = value;
+                    context.SaveChanges();
+                    status = true;
+                }
+                else
+                {
+                    message = "Error";
+                }
+            }
+                var response = new { value = value, status = status, message = message };
+            JObject obj = JObject.FromObject(response);
+            return Content(obj.ToString());
+
         }
     }
     #endregion
