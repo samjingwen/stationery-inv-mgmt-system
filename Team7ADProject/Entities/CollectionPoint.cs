579f8cd1d88b83fa127c822203409b087db3b9b8
jwjwjw123 <e0321129@u.nus.edu>
2019-01-15 18:40:07 +0800

    commit all

--- Team7ADProject/Entities/CollectionPoint.cs
+++ Team7ADProject/Entities/CollectionPoint.cs
@@ -21,6 +21,7 @@ namespace Team7ADProject.Entities
         [StringLength(50)]
         public string CollectionDescription { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
         public DateTime Time { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
