af9934efb266a76b3c1fe0491870d5122894253e
Chanylec <e0321097@u.nus.edu>
2019-01-17 14:21:04 +0800

    generate rpt

--- Team7ADProject/Controllers/GenerateReportController.cs
+++ Team7ADProject/Controllers/GenerateReportController.cs
@@ -21,8 +21,34 @@ namespace Team7ADProject.Controllers
             List<StringDoubleDPViewModel> dataPoints = new List<StringDoubleDPViewModel>();
 
             LogicDB context = new LogicDB();
-            var genRpt = context.TransactionDetail.GroupBy(x => new { x.Disbursement.DepartmentId }).
-                Select(y => new { DeptID = y.Key.DepartmentId, TotalAmt = y.Sum(z => (z.Quantity * z.UnitPrice)) });
+            //var genRpt = context.TransactionDetail.GroupBy(x => new { x.Disbursement.DepartmentId }).
+            //    Select(y => new { DeptID = y.Key.DepartmentId, TotalAmt = y.Sum(z => (z.Quantity * z.UnitPrice)) });
+            var genRpt = context.TransactionDetail.
+               Where(x => x.Disbursement.DepartmentId=="BUSI").
+               GroupBy(y => new { y.Disbursement.DepartmentId }).
+               Select(z => new { DeptID = z.Key.DepartmentId, TotalAmt = z.Sum(a => (a.Quantity * a.UnitPrice)) });
+
+            foreach (var i in genRpt)
+            {
+                dataPoints.Add(new StringDoubleDPViewModel(i.DeptID, (double)i.TotalAmt));
+            }
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+            return View();
+        }
+
+        [Authorize(Roles = "Store Manager, Store Supervisor")]
+        [HttpPost]
+        public ActionResult GenerateDashboard(DateTime? fromDateTP, DateTime? toDateTP)
+        {
+            List<StringDoubleDPViewModel> dataPoints = new List<StringDoubleDPViewModel>();
+            LogicDB context = new LogicDB();
+
+            var genRpt = context.TransactionDetail.
+                Where(x => x.TransactionDate >= fromDateTP && x.TransactionDate <= toDateTP).
+                GroupBy(y => new { y.Disbursement.DepartmentId }).
+                Select(z => new { DeptID = z.Key.DepartmentId, TotalAmt = z.Sum(a => (a.Quantity * a.UnitPrice))});
 
             foreach (var i in genRpt)
             {
