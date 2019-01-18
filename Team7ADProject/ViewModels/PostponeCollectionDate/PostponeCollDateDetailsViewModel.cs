bf201357feaf5514eac2940e1f0d8109e7baf454
Lynn Lynn Oo <Lynn Lynn Oo@LAPTOP-UHF46CF4>
2019-01-18 15:26:58 +0800

    1/2 done for Postpone collection Date

---
+++ Team7ADProject/ViewModels/PostponeCollectionDate/PostponeCollDateDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.ViewModels.PostponeCollectionDate
+{
+    public class PostponeCollDateDetailsViewModel
+    {
+        public string transactionID { get; set; }
+        public string itemID { get; set; }
+        public string itemName { get; set; }
+        public string quantity { get; set; }
+        public string remarks { get; set; }
+        public string transactionRef { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Team7ADProject/ViewModels/PostponeCollectionDate/PostponeCollDateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.ViewModels
+{
+    //Author:Lynn Lynn Oo
+    public class PostponeCollDateViewModel
+    {
+        #region Author:Lynn Lynn Oo
+        public string RequestID { get; set; }
+        public DateTime CollectionDate { get; set; }
+
+        #endregion
+    }
+
+}
