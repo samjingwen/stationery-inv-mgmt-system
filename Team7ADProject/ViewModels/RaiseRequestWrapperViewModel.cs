1b6edc623ee3a82b567b5597d6cd9d16876181d6
Li Heng Teh <e0320674@u.nus.edu>
2019-01-18 10:11:35 +0800

    merge

--- Team7ADProject/ViewModels/RaiseRequestWrapperViewModel.cs
+++ Team7ADProject/ViewModels/RaiseRequestWrapperViewModel.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace Team7ADProject.ViewModels
 {
-    public class RaiserRequestWrapperViewModel
+    public class RaiseRequestWrapperViewModel
     {
         public string Status { get; set; }
         public DateTime RequestDate { get; set; }
