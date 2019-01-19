4cdf7bc2dfa1009ad47089cfcc90b51e90a2d113
Lynn Lynn Oo <Lynn Lynn Oo@LAPTOP-UHF46CF4>
2019-01-19 16:51:28 +0800

    PostponeCollDateforDepartment

---
+++ Team7ADProject/Controllers/ManagePostponeCollectionDateDepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team7ADProject.Controllers
+{
+    public class ManagePostponeCollectionDateDepartmentController : Controller
+    {
+        // GET: ManagePostponeCollectionDateDepartment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
