9e0e82cc0ba6a147604b356794d98ba97bfbd3e4
jwjwjw123 <e0321129@u.nus.edu>
2019-01-20 20:44:22 +0800

    added some stuff

--- Team7ADProject/Controllers/ManagePointController.cs
+++ Team7ADProject/Controllers/ManagePointController.cs
@@ -35,10 +35,18 @@ namespace Team7ADProject.Controllers
         public ActionResult AddOrEdit(int id = 0)
         {
             ManagePointViewModel model = new ManagePointViewModel();
+            if (id != 0)
+            {
+                var query = db.CollectionPoint.Where(x => x.CollectionPointId == id).FirstOrDefault();
+                model.CollectionPointId = query.CollectionPointId;
+                model.CollectionDescription = query.CollectionDescription;
+                model.CPImagePath = query.CPImagePath;
+                model.Time = query.Time.ToString("hh:mm tt");
+            }
             return View(model);
         }
 
-        
+
         // POST: ManagePoint/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
@@ -46,167 +54,75 @@ namespace Team7ADProject.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult AddOrEdit(ManagePointViewModel collectionPoint)
         {
-            CollectionPoint cp = new CollectionPoint();
-            if (collectionPoint.ImageUpload != null)
+            try
             {
-                string fileName = Path.GetFileNameWithoutExtension(collectionPoint.ImageUpload.FileName);
-                string extension = Path.GetExtension(collectionPoint.ImageUpload.FileName);
-                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-                string date = DateTime.Now.ToString("yyyy/M/dd ");
-                cp = new CollectionPoint()
+                if (collectionPoint.ImageUpload == null && collectionPoint.CollectionPointId == 0)
                 {
-                    CollectionPointId = collectionPoint.CollectionPointId,
-                    CollectionDescription = collectionPoint.CollectionDescription,
-                    CPImagePath = "/Content/images/CollPoint/" + fileName,
-                    Time = DateTime.ParseExact(date + collectionPoint.Time, "yyyy/M/dd hh:mm tt", null)
-                };
-                collectionPoint.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/Content/images/CollPoint/"), fileName));
+                    throw new Exception("Please select an image.");
+                }
+                CollectionPoint cp = new CollectionPoint();
+                if (collectionPoint.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(collectionPoint.ImageUpload.FileName);
+                    string extension = Path.GetExtension(collectionPoint.ImageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    cp.CPImagePath = "/Content/images/CollPoint/" + fileName;
+                    collectionPoint.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/Content/images/CollPoint/"), fileName));
+                }
+
+                string date = DateTime.Now.ToString("yyyy/M/dd ");
+                cp.CollectionDescription = collectionPoint.CollectionDescription;
+                cp.Time = DateTime.ParseExact(date + collectionPoint.Time, "yyyy/M/dd h:mm tt", null);
+
+
+                if (collectionPoint.CollectionPointId == 0)
+                {
+                    db.CollectionPoint.Add(cp);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    var oldCP = db.CollectionPoint.Where(x => x.CollectionPointId == collectionPoint.CollectionPointId).FirstOrDefault();
+                    oldCP.CollectionDescription = cp.CollectionDescription;
+                    if (collectionPoint.ImageUpload != null)
+                        oldCP.CPImagePath = cp.CPImagePath;
+                    oldCP.Time = cp.Time;
+
+                    db.SaveChanges();
+                }
+
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", db.CollectionPoint.ToList()), message = "Submitted Successfully" }, JsonRequestBehavior.AllowGet);
+
             }
 
 
-            db.CollectionPoint.Add(cp);
-            db.SaveChanges();
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
 
-
-            return RedirectToAction("ViewAll");
+            }
         }
 
-        // GET: ManagePoint/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Delete(int id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CollectionPoint collectionPoint = db.CollectionPoint.Find(id);
-            ManagePointViewModel model = new ManagePointViewModel
-            {
-                CollectionPointId = collectionPoint.CollectionPointId,
-                CollectionDescription = collectionPoint.CollectionDescription,
-                Time = collectionPoint.Time.ToString("hh:mm tt")
-        };
-
-            if (collectionPoint == null)
-            {
-                return HttpNotFound();
-            }
-            return View(model);
-        }
-
-        // POST: ManagePoint/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CollectionPointId,CollectionDescription,Time")] CollectionPoint collectionPoint)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(collectionPoint).State = EntityState.Modified;
+                CollectionPoint cp = db.CollectionPoint.Where(x => x.CollectionPointId == id).FirstOrDefault();
+                db.CollectionPoint.Remove(cp);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", db.CollectionPoint.ToList()), message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
             }
-            return View(collectionPoint);
-        }
-
-        // GET: ManagePoint/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
+            catch (Exception ex)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
             }
-            CollectionPoint collectionPoint = db.CollectionPoint.Find(id);
-            if (collectionPoint == null)
-            {
-                return HttpNotFound();
-            }
-            return View(collectionPoint);
-        }
-
-        // POST: ManagePoint/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            CollectionPoint collectionPoint = db.CollectionPoint.Find(id);
-            db.CollectionPoint.Remove(collectionPoint);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
 
 
 
-        //public ActionResult Index()
-        //{
-        //    LogicDB context = new LogicDB();
-        //    var cpList = (from x in context.CollectionPoint select new ManagePointViewModel
-        //    {
-        //        CollectionPointId = x.CollectionPointId,
-        //        CollectionDescription = x.CollectionDescription,
-        //        Time = x.Time.ToString()
-        //        }).AsEnumerable();
-        //    return View(cpList);
-        //}
 
 
-        ////public ActionResult GetData()
-        ////{
-        ////    LogicDB context = new LogicDB();
-        ////    var cpList = (from x in context.CollectionPoint select new { x.CollectionPointId, x.CollectionDescription, x.Time }).ToList();
-        ////    return Json(new { data = cpList }, JsonRequestBehavior.AllowGet);
-        ////}
 
-        //public ActionResult SaveCP(int id, string propertyName, string value)
-        //{
-        //    var status = false;
-        //    var message = "";
-
-        //    //Update data to database
-        //    using (LogicDB context = new LogicDB())
-        //    {
-        //        var CollPoint = context.CollectionPoint.Find(id);
-
-        //        object updateValue = value;
-                
-        //        if(propertyName == "Time")
-        //        {
-        //            string date = DateTime.Now.ToString("yyyy/M/dd ");
-        //            DateTime time = DateTime.ParseExact(date + value, "yyyy/M/dd HH:mm", null);
-        //            updateValue = time;
-        //        }
-
-        //        if (CollPoint != null)
-        //        {
-        //            context.Entry(CollPoint).Property(propertyName).CurrentValue = updateValue;
-        //            context.SaveChanges();
-        //            status = true;
-        //        }
-        //        else
-        //        {
-        //            message = "Error";
-        //        }
-        //    }
-        //    var response = new { value = value, status = status, message = message };
-        //    JObject obj = JObject.FromObject(response);
-        //    return Content(obj.ToString());
-
-        //}
-
-        //public ActionResult AddCP()
-        //{
-        //    return View(new ManagePointViewModel());
-
-        //}
 
 
     }
