01cb1bba2298b2bf6578364f7546982dd0200c31
宗培 程 <1015440098@qq.com>
2019-01-20 23:02:57 +0800

    raise adjustment

---
+++ Team7ADProject/ViewModels/Api/AdjustmentApiViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.ViewModels.Api
+{
+    public class AdjustmentApiViewModel
+    {
+        public string UnitOfMeature;
+
+        public int QuantityWareHouse;
+
+        public decimal Price;
+    }
+}
