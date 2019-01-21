d023fae2bcfedb33966fa1ff16554c2093c17735
jwjwjw123 <e0321129@u.nus.edu>
2019-01-22 00:50:42 +0800

    deleted stuff

--- Team7ADProject/Models/IdentityModels.cs
+++ Team7ADProject/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,9 @@ namespace Team7ADProject.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string EmployeeName { get; set; }
+        public string DepartmentId { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
