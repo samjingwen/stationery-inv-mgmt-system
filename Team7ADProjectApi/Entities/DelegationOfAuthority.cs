80da45265ed536118801ee130403c3ffaa1a1623
jwjwjw123 <e0321129@u.nus.edu>
2019-01-21 13:33:31 +0800

    commit changes

---
+++ Team7ADProjectApi/Entities/DelegationOfAuthority.cs
@@ -0,0 +1,39 @@
+namespace Team7ADProjectApi.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("DelegationOfAuthority")]
+    public partial class DelegationOfAuthority
+    {
+        [Key]
+        public int DOAId { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string DelegatedBy { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string DelegatedTo { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime StartDate { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public string DepartmentId { get; set; }
+
+        public virtual AspNetUsers AspNetUsers { get; set; }
+
+        public virtual AspNetUsers AspNetUsers1 { get; set; }
+
+        public virtual Department Department { get; set; }
+    }
+}
---
+++ Team7ADProjectApi/Entities/PurchaseOrder.cs
@@ -0,0 +1,56 @@
+namespace Team7ADProjectApi.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("PurchaseOrder")]
+    public partial class PurchaseOrder
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public PurchaseOrder()
+        {
+            DeliveryOrder = new HashSet<DeliveryOrder>();
+            TransactionDetail = new HashSet<TransactionDetail>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string PONo { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public string SupplierId { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string OrderedBy { get; set; }
+
+        [StringLength(128)]
+        public string ApprovedBy { get; set; }
+
+        [Column(TypeName = "numeric")]
+        public decimal Amount { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Status { get; set; }
+
+        public virtual AspNetUsers AspNetUsers { get; set; }
+
+        public virtual AspNetUsers AspNetUsers1 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<DeliveryOrder> DeliveryOrder { get; set; }
+
+        public virtual Supplier Supplier { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<TransactionDetail> TransactionDetail { get; set; }
+    }
+}
