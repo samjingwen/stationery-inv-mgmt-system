e940fb18234c3b2e0bd95859e8a242183edcf5c5
Lynn Lynn Oo <Lynn Lynn Oo@LAPTOP-UHF46CF4>
2019-01-21 21:07:14 +0800

    DONE!! 😁

--- Team7ADProject/Controllers/ManagePostponeCollectionDateController.cs
+++ Team7ADProject/Controllers/ManagePostponeCollectionDateController.cs
@@ -12,50 +12,41 @@ namespace Team7ADProject.Controllers
     public class ManagePostponeCollectionDateController : Controller
     {
         LogicDB _context;
-
+        #region Author:Lynn Lynn Oo
         public ManagePostponeCollectionDateController()
         {
             _context = new LogicDB();
         }
         // GET: ManagePostponeCollectionDate
+        [Authorize(Roles = "Store Supervisor")]
         public ActionResult Index()
         {
-            string userid = User.Identity.GetUserId();
-            string depId = _context.AspNetUsers.Where(x => x.Id == userid).Select(x => x.DepartmentId).First();
-            List<StationeryRequest> pendingDisbursements = _context.StationeryRequest.Where(x => x.Status == "Pending Disbursement" && x.DepartmentId==depId).ToList();
+            //string userid = User.Identity.GetUserId();
+            //string depId = _context.AspNetUsers.Where(x => x.Id == userid).Select(x => x.DepartmentId).First();
+            //Store Supervisor can see all the Pending Disbursement of all depts
+            List<StationeryRequest> pendingDisbursements = _context.StationeryRequest.Where(x => x.Status == "Pending Disbursement" /*&& x.DepartmentId==depId*/).ToList();
             return View(pendingDisbursements);
 
         }
 
-        //GET: ManagePostponeCollectionDate/D
+        //GET: ManagePostponeCollectionDate/Details
+       
         public ActionResult Details(string id)
         {
             List<TransactionDetail> ItemsByID = _context.TransactionDetail.Where(x => x.TransactionRef == id).ToList();
             return View(ItemsByID);
         }
 
-        public ActionResult Postpone(string id, PostponeCollDateViewModel model)
+        public ActionResult Postpone(string id)
         {
-            string i = id;
-            LogicDB context = new LogicDB();
-            StationeryRequest dd = new StationeryRequest();
-            dd.CollectionDate = model.CollectionDate;//new DateTime(2017,3,5)
             StationeryRequest current = _context.StationeryRequest.First(m => m.RequestId == id);
             current.CollectionDate = current.CollectionDate.Value.AddDays(7);
             _context.SaveChanges();
-            return View();
+            return RedirectToAction("Index");
         }
+        #endregion
     }
-
-//    public string ChangeDate(PostponeCollDateViewModel model)
-//    {
-
-//        LogicDB context = new LogicDB();
-//        StationeryRequest dd = new StationeryRequest();       
-//        dd.CollectionDate = model.CollectionDate;//new DateTime(2017,3,5)
-//        return "success";
-
-//    }
-//}
 }
+
+
 
