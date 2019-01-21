e8ddbc98b7ae434deb04a7941ef12f9b339226b1
KittySwe <kaythiswetun@gmail.com>
2019-01-21 16:13:16 +0800

    commit missing

--- Team7ADProjectApi/Controllers/DepartmentHeadController.cs
+++ Team7ADProjectApi/Controllers/DepartmentHeadController.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Team7ADProjectApi.ViewModels;
 
 namespace Team7ADProjectApi.Controllers
 {
     public class DepartmentHeadController : ApiController
     {
+
+        [Authorize(Roles = "Department Head")]
+        [HttpGet]
+        [Route("api/managedepartmentRep/{id}")]
+        public IEnumerable<BriefDepartment> GetDepartments()
+        {
+            GlobalClass gc = new GlobalClass();
+            return gc.ListDepartment(id);
+        }
+
     }
 }
