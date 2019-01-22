e8831dc92ec773143f9836d9789e6a166e8c5c4e
KittySwe <kaythiswetun@gmail.com>
2019-01-22 10:40:21 +0800

    add api dep rep

---
+++ Team7ADProjectApi/ViewModels/DepEmp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProjectApi.ViewModels
+{
+    public class DepEmp
+    {
+       public string EName { get; set; }
+       public string Empid { get; set; }
+        public string Email { get; set; }
+        public string phone { get; set; }
+
+    }
+}
