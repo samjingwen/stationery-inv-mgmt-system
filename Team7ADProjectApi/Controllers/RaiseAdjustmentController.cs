4bd1112dec4bf7ee8ee5becf4fe9b31f03b948c3
宗培 程 <1015440098@qq.com>
2019-01-22 15:58:40 +0800

    raise adjustment

--- Team7ADProjectApi/Controllers/RaiseAdjustmentController.cs
+++ Team7ADProjectApi/Controllers/RaiseAdjustmentController.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Team7ADProjectApi.Entities;
 
 namespace Team7ADProjectApi.Controllers
 {
     public class RaiseAdjustmentController : ApiController
     {
+        public LogicDB context = new LogicDB();
+
+        [HttpGet]
+        [Route("api/categories")]
+        public IHttpActionResult getCategories()
+        {
+            List<string> results = context.Stationery.Select(x => x.Category).Distinct().ToList();
+            return Ok(results);
+        }
     }
 }
