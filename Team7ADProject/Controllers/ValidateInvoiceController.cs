51e0f9442682462c4bf7072a4282126474d742fa
gaojiaxue <gaojiaxue@outlook.com>
2019-01-22 10:36:33 +0800

    comments

--- Team7ADProject/Controllers/ValidateInvoiceController.cs
+++ Team7ADProject/Controllers/ValidateInvoiceController.cs
@@ -8,7 +8,7 @@ using Team7ADProject.ViewModels;
 
 namespace Team7ADProject.Controllers
 {
-    #region Gao Jiaxue
+    #region Author:Gao Jiaxue
     public class ValidateInvoiceController : Controller
     {
         //get Data
@@ -37,6 +37,7 @@ namespace Team7ADProject.Controllers
             return View(invoiceDetailsViewModel);
         }
 
+        //Create new Invoice and TransactionDetails
         [HttpPost]
         [Authorize(Roles = "Store Clerk")]
         public ActionResult Create(Invoice[] invoice, TransactionDetail[] requests)
@@ -48,6 +49,7 @@ namespace Team7ADProject.Controllers
             { result = "Your DelOrder No is wrong!"; }
             if (invoice != null && requests != null&&delID)
             {
+                //Create Invoice
                 Invoice newInvoice = new Invoice();
                 newInvoice.InvoiceId = invoiceID;
                 newInvoice.InvoiceNo = invoice[0].InvoiceNo;
