1a5fa9bb4d48f10f537259b0001254f6d80928e9
jwjwjw123 <e0321129@u.nus.edu>
2019-01-22 10:50:03 +0800

    modularise retrieval

--- Team7ADProject/Service/StationeryRequestService.cs
+++ Team7ADProject/Service/StationeryRequestService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Team7ADProject.Entities;
+using Team7ADProject.ViewModels;
 
 namespace Team7ADProject.Service
 {
@@ -29,6 +31,47 @@ namespace Team7ADProject.Service
             }
         }
         #endregion
+
+        LogicDB context = new LogicDB();
+
+
+        public List<RequestByItemViewModel> GetListRequestByItem()
+        {
+            List<RequestByItemViewModel> model = new List<RequestByItemViewModel>();
+            var query = context.RequestByItemView.OrderBy(x => x.ItemId).ToList();
+            foreach (var i in query)
+            {
+                var item = model.Find(x => x.ItemId == i.ItemId);
+                var disb = context.DisbByDept.Where(x => x.ItemId == i.ItemId && x.DepartmentId == i.DepartmentId).FirstOrDefault();
+                if (item != null)
+                {
+                    var newModel = new BreakdownByDeptViewModel
+                    {
+                        DepartmentId = i.DepartmentId,
+                        DepartmentName = i.DepartmentName,
+                        Quantity = disb == null ? (int)i.Quantity : ((int)i.Quantity - (int)disb.Quantity)
+                    };
+                    item.requestList.Add(newModel);
+                }
+                else
+                {
+                    RequestByItemViewModel requestByItemViewModel = new RequestByItemViewModel();
+                    requestByItemViewModel.ItemId = i.ItemId;
+                    requestByItemViewModel.Description = i.Description;
+                    requestByItemViewModel.requestList = new List<BreakdownByDeptViewModel>();
+                    requestByItemViewModel.requestList.Add(new BreakdownByDeptViewModel
+                    {
+                        DepartmentId = i.DepartmentId,
+                        DepartmentName = i.DepartmentName,
+                        Quantity = disb == null ? (int)i.Quantity : ((int)i.Quantity - (int)disb.Quantity)
+                    });
+                    model.Add(requestByItemViewModel);
+                }
+            }
+            return model;
+        }
+
+
 
 
     }
