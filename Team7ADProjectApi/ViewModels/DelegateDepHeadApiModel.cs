cde8cb1ed0bf7ae3c1070ce6148e97e085cac388
Li Heng Teh <e0320674@u.nus.edu>
2019-01-22 10:27:04 +0800

    merge changes web API for Delegate

---
+++ Team7ADProjectApi/ViewModels/DelegateDepHeadApiModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProjectApi.ViewModels
+{
+    public class DelegateDepHeadApiModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string UserId { get; set; }
+        public string DepartmentHeadName { get; set; }
+    }
+}
