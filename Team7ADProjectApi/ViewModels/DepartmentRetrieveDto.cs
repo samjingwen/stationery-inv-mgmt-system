a16ac91589a222a93b1471327276281be0085583
Li Heng Teh <e0320674@u.nus.edu>
2019-01-23 19:08:58 +0800

    clerk controller

---
+++ Team7ADProjectApi/ViewModels/DepartmentRetrieveDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProjectApi.ViewModels
+{
+    public class DepartmentRetrieveDto
+    {
+        public IEnumerable<StationeryRetrieveDto> stationeryList { get; set; }
+    }
+}
---
+++ Team7ADProjectApi/ViewModels/StationeryRetrieveDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProjectApi.ViewModels
+{
+    public class StationeryRetrieveDto
+    {
+        public string ItemName { get; set; }
+        public string Location { get; set; }
+        public int Quantity { get; set; }
+        public int ActualRetrieved { get; set; }
+        public bool Collected { get; set; }
+    }
+}
