734068b6139e7e06d4e5f561abfe11b6858e3e0f
Chanylec <e0321097@u.nus.edu>
2019-01-23 10:36:53 +0800

    cosmetic changes

--- Team7ADProject/Controllers/AccountController.cs
+++ Team7ADProject/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace Team7ADProject.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
