59e4379d44c19f44f8d78328bf1c4c036d8af433
jwjwjw123 <e0321129@u.nus.edu>
2019-01-23 11:05:03 +0800

    rf dh

--- Team7ADProject/Controllers/DelegateHeadController.cs
+++ Team7ADProject/Controllers/DelegateHeadController.cs
@@ -8,67 +8,50 @@ using Team7ADProject.Entities;
 using Team7ADProject.Models;
 using Microsoft.AspNet.Identity.Owin;
 using Team7ADProject.ViewModels;
+using Team7ADProject.Service;
 
 namespace Team7ADProject.Controllers
 {
+    #region Author: Kay Thi Swe Tun
+
     //For DH to delegate ADH
+    [RoleAuthorize(Roles = "Department Head")]
     public class DelegateHeadController : Controller
     {
-        private LogicDB _context;
+        GeneralMgmtService gmService = GeneralMgmtService.Instance;
 
-        public DelegateHeadController()
-        {
-            _context = new LogicDB();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            _context.Dispose();
-        }
-
-        #region Author: Kay Thi Swe Tun
         // GET: DelegateHead
-        [Authorize(Roles = RoleName.DepartmentHead)]
+        [HttpGet]
         public ActionResult Index()
         {
             string userId = User.Identity.GetUserId();
             DelegateHeadViewModel viewModel = new DelegateHeadViewModel(userId);
-
+            string[] delHead = gmService.GetDelegatedHead(userId);
+            if (delHead != null)
+            {
+                ViewBag.DelHead = delHead;
+            }
             ViewBag.DepName = viewModel.DepartmentName;
             return View(viewModel);
         }
 
+
         [HttpPost]
-        [Authorize(Roles = RoleName.DepartmentHead)]
-        public string Delegate(DelegateHeadViewModel model)
+        public ActionResult DelegateHead(DelegateHeadViewModel model)
         {
+            return RedirectToAction("Index", "Home");
             if (!ModelState.IsValid)
             {
-                return "fail";
+                return RedirectToAction("Index", "Home");
             }
-
             else
             {
                 string userId = User.Identity.GetUserId();
-                var selectedEmployee = model.SelectedUser;
-                DelegationOfAuthority doaInDb = new DelegationOfAuthority
-                {
-                    DelegatedBy = userId,//"b36a58f3-51f9-47eb-8601-bcc757a8cadb";//selected Employee ID;
-                    DelegatedTo = selectedEmployee,
-                    StartDate = model.StartDate,//new DateTime(2017,3,5);
-                    EndDate = model.EndDate,//new DateTime(2017, 5, 5);
-                    DepartmentId = model.DepartmentID
-                };
+                string selectedUser = model.SelectedUser;
 
-                //model.CurrentUser = userId;
+                gmService.AssignDelegateHead(userId, selectedUser, model.DepartmentId, model.StartDate, model.EndDate);
 
-                ApplicationUserManager userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-
-                userManager.RemoveFromRole(doaInDb.DelegatedTo, RoleName.Employee);
-                userManager.AddToRole(doaInDb.DelegatedTo, RoleName.ActingDepartmentHead);
-                _context.DelegationOfAuthority.Add(doaInDb);
-                _context.SaveChanges();
-                return "success";
+                return RedirectToAction("About", "Home");
             }
         }
     }
--- Team7ADProject/Service/GeneralMgmtService.cs
+++ Team7ADProject/Service/GeneralMgmtService.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Team7ADProject.Entities;
+using Team7ADProject.Models;
 
 namespace Team7ADProject.Service
 {
@@ -30,6 +34,39 @@ namespace Team7ADProject.Service
         }
         #endregion
 
+        LogicDB context = new LogicDB();
 
+        public void AssignDelegateHead(string userId, string selectedUser, string deptId, DateTime startDate, DateTime endDate)
+        {
+            DelegationOfAuthority doaInDb = new DelegationOfAuthority
+            {
+                DelegatedBy = userId,//"b36a58f3-51f9-47eb-8601-bcc757a8cadb";//selected Employee ID;
+                DelegatedTo = selectedUser,
+                StartDate = startDate,//new DateTime(2017,3,5);
+                EndDate = endDate,//new DateTime(2017, 5, 5);
+                DepartmentId = deptId
+            };
+
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var currentUser = manager.FindById(userId);
+
+            manager.RemoveFromRole(selectedUser, RoleName.Employee);
+            manager.AddToRole(selectedUser, RoleName.ActingDepartmentHead);
+            context.DelegationOfAuthority.Add(doaInDb);
+            context.SaveChanges();
+        }
+
+        public string[] GetDelegatedHead(string userId)
+        {
+            var query = context.DelegationOfAuthority.Where(x => x.EndDate >= DateTime.Now && x.DelegatedBy == userId).FirstOrDefault();
+            if (query == null)
+            {
+                return null;
+            }
+            else
+            {
+                return new string[] { query.AspNetUsers1.Id, query.AspNetUsers1.EmployeeName };
+            }
+        }
     }
 }
