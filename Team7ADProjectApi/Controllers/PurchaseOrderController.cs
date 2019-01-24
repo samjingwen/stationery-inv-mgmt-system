27020d9c21558970326116a6802ae7d85baa6b41
Mr-ZTK <zantunkhine@googlemail.com>
2019-01-25 01:45:49 +0800

    Edited API for PO

--- Team7ADProjectApi/Controllers/PurchaseOrderController.cs
+++ Team7ADProjectApi/Controllers/PurchaseOrderController.cs
@@ -21,7 +21,7 @@ namespace Team7ADProjectApi.Controllers
         #region List all the POs   
 
         [HttpGet]
-        [Route("api/allpo/")]
+        [Route("api/allpo")]
         public IEnumerable<PendingPO> ListAllPO()
         {
             GlobalClass gc = new GlobalClass();
@@ -32,7 +32,7 @@ namespace Team7ADProjectApi.Controllers
 
         #region List all the pending POs
         [HttpGet]
-        [Route("api/pendingpo/")]
+        [Route("api/pendingpo")]
         public IEnumerable<PendingPO> ListAllPendingPO()
         {
             GlobalClass gc = new GlobalClass();
