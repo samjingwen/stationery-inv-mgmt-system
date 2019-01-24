a376504df47a438dd72ea98aeae4efe6e0f99500
jwjwjw123 <e0321129@u.nus.edu>
2019-01-24 09:57:26 +0800

    add all

---
+++ Team7ADProjectAPI/ViewModels/DisbursementListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProjectApi.ViewModels
+{
+    public class DisbursementListViewModel
+    {
+        public string DisbursementNo { get; set; }
+        public string DepartmentId { get; set; }
+
+
+
+
+    }
+}
