27dd6a71c3c6065cc76b8615414e8c0b7001e01e
gaojiaxue <gaojiaxue@outlook.com>
2019-01-25 12:52:10 +0800

    stationery request

---
+++ Team7ADProjectAPI/ViewModels/StationeryRequestApiModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Team7ADProjectApi.Entities;
+
+namespace Team7ADProjectApi.ViewModels
+{
+    public class StationeryRequestApiModel
+    {
+       public String RequestId { get; set; }
+       public String RequestedBy { get; set; }
+       public String ApprovedBy { get; set; }
+       public String DepartmentId { get; set; }
+        public String Status { get; set; }
+        public DateTime RequestDate { get; set; }
+        public List<TransactionDetail> transactionDetailsList { get; set; }
+    }
+}
