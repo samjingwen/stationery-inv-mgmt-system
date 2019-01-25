c20078a0f0f5e6de25e1fd6c3e46403598c3bc6b
Li Heng Teh <e0320674@u.nus.edu>
2019-01-26 03:15:53 +0800

    added logic for retrieval and set retrieval

---
+++ Team7ADProjectAPI/ViewModels/StockAdjustmentValidate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.ViewModels
+{
+    public class StockAdjustmentCompare
+    {
+        public int RequestedQty { get; set; }
+        public int RetrievedQty { get; set; }
+        public int WarehouseQty { get; set; }
+    }
+}
