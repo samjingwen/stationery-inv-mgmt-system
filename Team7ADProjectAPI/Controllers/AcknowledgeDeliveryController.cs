026c017f0fe47ddd34824896be154121ab7ddd34
Mr-ZTK <zantunkhine@googlemail.com>
2019-01-25 14:57:08 +0800

    Added API for Ack Delivery

---
+++ Team7ADProjectAPI/Controllers/AcknowledgeDeliveryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Team7ADProjectApi.ViewModels;
+
+//Author Zan Tun Khine
+
+namespace Team7ADProjectApi.Controllers
+{
+    public class AcknowledgeDeliveryController : ApiController
+    {
+        #region Author Zan Tun Khine
+
+        #region Display Detail list of items for selected PO
+        [HttpGet]
+        [Route("api/ackdelivery/{*poNo}")]
+        public IEnumerable<PendingPODetails> ListAllPendingPODetails(string poNo)
+        {
+            GlobalClass gc = new GlobalClass();
+            return gc.SelectedPODetails(poNo);
+        }
+        #endregion
+
+
+
+        #endregion
+    }
+}
---
+++ Team7ADProjectAPI/ViewModels/AckDelivery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProjectApi.ViewModels
+{
+
+    #region Author Zan Tun Khne
+    public class AckDelivery
+    {
+        #region From Delivery Order
+
+        public string DelOrderId { get; set; }
+        public string DelOrderNo { get; set; }
+        public string SupplierId { get; set; }
+        public string AcceptedBy { get; set; }
+        public DateTime Date { get; set; }
+        public string PONo { get; set; }
+        public string Status { get; set; }
+
+        #endregion
+
+        #region From Stationery 
+
+        public string ItemId { get; set; }
+        public int QuantityWarehouse { get; set; }
+
+        #endregion
+
+        #region From TrasactionDetail
+
+        public int TransactionId { get; set; }
+        public int Quantity { get; set; }
+        public string Remarks { get; set; }
+        public string TransactionRef { get; set; }    
+
+        #endregion
+
+
+
+    }
+
+    #endregion
+}
