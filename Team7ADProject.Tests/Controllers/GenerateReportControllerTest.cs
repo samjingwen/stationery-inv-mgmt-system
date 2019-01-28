e25ba6a68bcfd159b2ab13a2660fa759edccd9db
Chanylec <e0321097@u.nus.edu>
2019-01-28 12:53:00 +0800

    test scripts

---
+++ Team7ADProject.Tests/Controllers/GenerateReportControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Team7ADProject.Controllers;
+using Team7ADProject.ViewModels.GenerateReport;
+
+namespace Team7ADProject.Tests.Controllers
+{
+    [TestClass]
+    public class GenerateReportControllerTest
+    {
+        [TestMethod]
+        public void TestInitialReportView()
+        {
+            var controller = new GenerateReportController();
+            var result = controller.GenerateDashboard() as ViewResult;
+            var model = result.Model as GenerateReportViewModel;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Disbursements", model.module);
+            Assert.AreEqual(10, model.selectentcategory.Count);
+        }
+
+        [TestMethod]
+        public void TestFilteredReportView()
+        {
+            var controller = new GenerateReportController();
+            var result = controller.GenerateDashboard(new DateTime(2017, 03, 03), new DateTime(2018, 03, 03), "Requests",
+                new List<String>(new string[] { "Pen","Clip" }), new List<String>(new string[] { "ACCT","BUSI" }), null, null) 
+                as ViewResult;
+            var model = result.Model as GenerateReportViewModel;
+                       
+            Assert.IsNotNull(result);
+            
+
+        }
+    }
+}
