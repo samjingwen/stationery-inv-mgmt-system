b145c417fb5ca283fcab13600a2d607560046401
Lynn Lynn Oo <Lynn Lynn Oo@LAPTOP-UHF46CF4>
2019-01-28 10:09:14 +0800

    added route

---
+++ Team7ADProject/ViewModels/Api/ReturntoWarehouseApiViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.ViewModels.Api
+{
+    public class ReturntoWarehouseApiViewModel
+    {
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string Department { get; set; }
+        public string Location { get; set; }
+    }
+}
