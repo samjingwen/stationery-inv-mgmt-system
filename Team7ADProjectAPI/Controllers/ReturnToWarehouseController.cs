294ff5474421b6dd1356b1120be3328aa78557b3
Lynn Lynn Oo <Lynn Lynn Oo@LAPTOP-UHF46CF4>
2019-01-30 16:23:34 +0800

    List Api

--- Team7ADProjectAPI/Controllers/ReturnToWarehouseController.cs
+++ Team7ADProjectAPI/Controllers/ReturnToWarehouseController.cs
@@ -5,27 +5,52 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Team7ADProjectApi.ViewModels;
+using Team7ADProjectApi.Entities;
 
 namespace Team7ADProjectApi.Controllers
 {
     public class ReturnToWarehouseController : ApiController
     {
+        LogicDB _context;
+
+        public ReturnToWarehouseController()
+        {
+            _context = new LogicDB();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         #region Author:Lynn Lynn Oo
 
         [Route("~/api/returntowarehouse/getitemlist")]
         [HttpGet]
         public IHttpActionResult GetItemList()
         {
-            GlobalClass gc = new GlobalClass();
-            return Ok(gc.GetItemList());
+            //GlobalClass gc = new GlobalClass();
+            //return Ok(gc.GetItemList());
+            List<ReturntoWarehouseApiModel> apiModels = new List<ReturntoWarehouseApiModel>();
+            List<StationeryRequest> requests = _context.StationeryRequest.Where(m => m.Status == "Void" || m.Status == "Partially Returned").ToList();
+            foreach (StationeryRequest current in requests)
+            {
+                ReturntoWarehouseApiModel apiModel = new ReturntoWarehouseApiModel
+                {
+                    RequestId = current.RequestId,
+                    TransactionDetails = current.TransactionDetail.ToList()
+                };
+                apiModels.Add(apiModel);
+            }
+            return Ok(apiModels);
         }
 
         [HttpPost]
-        [Route("~/api/returntowarehouse/return")]
-        public IHttpActionResult ReturnItem(ReturntoWarehouseApiModel apiModel)
+        [Route("api/returntowarehouse/return")]
+        public IHttpActionResult ReturnItem([FromBody]ReturntoWarehouseApiModel apiModel)
         {
             GlobalClass gc = new GlobalClass();
-            string status = gc.Return(apiModel);
+            string status = gc.ReturnTo(apiModel);
             return Ok(status);
         }
 
--- Team7ADProjectAPI/ViewModels/ReturntoWarehouseApiModel.cs
+++ Team7ADProjectAPI/ViewModels/ReturntoWarehouseApiModel.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Team7ADProjectApi.Entities;
 
 namespace Team7ADProjectApi.ViewModels
 {
     public class ReturntoWarehouseApiModel
     {
-        public string RequestId { get; set; }
-        public string ItemId { get; set; }
-        public string Description { get; set; }
-        public int Quantity { get; set; }
-        public string Department { get; set; }
-        public string Location { get; set; }
+        public string RequestId { get; set; } 
+        public List<TransactionDetail> TransactionDetails { get; set; }
 
     }
 }
