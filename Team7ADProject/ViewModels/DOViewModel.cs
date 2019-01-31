a12f7688230097dbe1cdad40dadd352359c9bca7
jwjwjw123 <e0321129@u.nus.edu>
2019-01-31 23:01:57 +0800

    val inv

---
+++ Team7ADProject/ViewModels/DOViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.ViewModels
+{
+    public class DOViewModel
+    {
+        [Required]
+        public string InvoiceNo { get; set; }
+        [Required]
+        public string SupplierId { get; set; }
+        [Required]
+        public decimal InvoiceAmount { get; set; }
+        [Required]
+        public string DelOrderNo { get; set; }
+    }
+
+    public class ItemsAndQtyViewModel
+    {
+        public string ItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
---
+++ Team7ADProject/ViewModels/ValidateInvoiceViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.ViewModels
+{
+    public class DOViewModel
+    {
+        [Required]
+        public string InvoiceNo { get; set; }
+        [Required]
+        public string SupplierId { get; set; }
+        [Required]
+        public List<string> DelOrderNo { get; set; }
+    }
+
+    
+}
